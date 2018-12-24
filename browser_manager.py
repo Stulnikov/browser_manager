3fb9e7c0fd007267c0b731bb9ac3a5d4e08ac06c
Aleksey <stualeich@gmail.com>
2018-12-24 16:12:04 +0200

    Renew flow for initializing driver

--- browser_manager.py
+++ browser_manager.py
@@ -1,3 +1,5 @@
+import time
+from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.remote.webdriver import WebDriver
 
@@ -60,9 +62,41 @@ class BaseBrowser():
         del self.__driver.driver
 
 
-class Browser(BaseBrowser, metaclass=Singleton):
-    def __init__(self):
-        super().__init__()
+# class Browser(BaseBrowser, metaclass=Singleton):
+#     def __init__(self):
+#         super().__init__()
+
+class Browser(metaclass=Singleton):
+    __driver = None
+
+    def open_browser(self, driver):
+        self._validate(driver)
+        self.__driver = driver
+
+    @property
+    def driver(self) -> WebDriver:
+        if not self.__driver:
+            raise WebDriverException('The browser is not open')
+        return self.__driver
+
+    def close_browser(self):
+        self.__driver.quit()
+        self.__driver = None
+
+    def _validate(self, driver):
+        def close_browsers_with_exception(message):
+            try:
+                self.__driver.quit()
+                driver.quit()
+            except:
+                pass
+            raise WebDriverException(message)
+
+        if not isinstance(driver, WebDriver):
+            close_browsers_with_exception('The attribute passed must be WebDriver')
+
+        if self.__driver:
+            close_browsers_with_exception('The browser is already open')
 
 
 class Browsers(metaclass=Singleton):
@@ -103,3 +137,44 @@ class Browsers(metaclass=Singleton):
     def close_browsers(self):
         self.close_first_browser()
         self.close_second_browser()
+
+
+class BrowserManager(metaclass=Singleton):
+    __browsers = {}
+    __default_browser = None
+
+    def open_browser(self, web_driver: WebDriver, browser_name='default'):
+        if not isinstance(web_driver, WebDriver):
+            self.close_browsers()
+            raise WebDriverException('The attribute passed must be WebDriver')
+
+        if browser_name in self.__browsers:
+            self.__browsers['duplicate'] = web_driver
+            self.close_browsers()
+            raise WebDriverException('The browser is already open')
+
+        self.__browsers[browser_name] = web_driver
+        self.__default_browser = browser_name
+
+    @property
+    def driver(self) -> WebDriver:
+        if not self.__browsers:
+            raise WebDriverException('The browser is not open')
+
+        return self.__browsers[self.__default_browser]
+
+    def switch_to(self, browser_name):
+        if browser_name not in self.__browsers:
+            raise WebDriverException('The browser is not open')
+
+        self.__default_browser = browser_name
+
+    def close_browser(self, browser_name='default'):
+        if browser_name not in self.__browsers:
+            raise WebDriverException('The browser is not open')
+
+        self.__browsers[browser_name].quit()
+
+    def close_browsers(self):
+        for key, value in self.__browsers.items():
+            self.__browsers[key].quit()
